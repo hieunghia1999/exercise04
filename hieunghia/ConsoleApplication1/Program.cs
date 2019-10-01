e8312533acab71b598dae531a2d8212a6dd576ff
hieunghia <hieungghia1999@gmail.com>
2019-10-01 18:00:08 +0700

    1/10/2019

--- hieunghia/ConsoleApplication1/Program.cs
+++ hieunghia/ConsoleApplication1/Program.cs
@@ -6,11 +6,39 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Hell Github")
+            Console.WriteLine("Hello Github");
+        }
+        // input: float x
+        // output: int a that is smallest but not smaller than x
+        // idea: a = (int) x then a = a+1
+        // if x is negative then a is unchanged
+        // if x == a then a is unchanged
+
+        public static int Ceil (float x)
+        {
+            int a = (int) x;
+            if (x > 0)
+                a = a +1;
+            return a;
+        }
+        // input : float x
+        // output : int a that is largest but not bigger than x
+        // idea : a = (int) x
+        // if x is negative then a = a-1
+        // if x == a then a is unchanged 
+
+       public static int Floor(float x)
+        {
+           int a = (int)x;
+           if (x < 0)
+               if (x == a)
+            return 0;
+        }
+
         }
     }
 }
--- hieunghia/UnitTestProject1/Properties/AssemblyInfo.cs
+++ hieunghia/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("UnitTestProject1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("UnitTestProject1")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("cf06fa72-f8da-4d2a-93c7-c91d4d3a116d")]
+[assembly: Guid("e4a18097-2fd9-497a-994b-6b5ddd12749f")]
 
 // Version information for an assembly consists of the following four values:
 //
