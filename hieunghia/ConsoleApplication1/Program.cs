40b6c487a359a45515832f7f4a125c566c474568
Nguyen Hieu Nghia <hieungghia1999@gmail.com>
2019-10-03 13:04:37 +0700

    3/10/2019

--- hieunghia/ConsoleApplication1/Program.cs
+++ hieunghia/ConsoleApplication1/Program.cs
@@ -1,220 +1,123 @@
 ﻿using System;
-
 using System.Collections.Generic;
-
 using System.Linq;
-
 using System.Text;
-
 using System.Threading.Tasks;
 
-
-
- public namespace Exercises04
+namespace ConsoleApplication1
 {
-
     public class Program
     {
-
         static void Main(string[] args)
         {
+            Console.WriteLine("hello github");
+            Console.WriteLine(Square(4));
+            Console.WriteLine(Pow(2, 3));
+            Console.WriteLine(IsPrime(8));
+            Console.WriteLine(IsEven(3));
+            Console.WriteLine(Floor(5.7F));
+            Console.WriteLine(Cube(-10));
+            Console.WriteLine(Ceil(4.3F));
+            Console.WriteLine(Abs(6));
+            Console.WriteLine(Factorial(20L));
+            Console.WriteLine("rand1: " + Rand1());
+            Console.WriteLine("rand2: " + Rand2());
 
-            //Console.WriteLine("Hello Everybody");
-
-            //Console.WriteLine(IsOdd(3));
-
-            //Console.WriteLine(IsEven(4));
-
-            //Console.WriteLine(IsPrime(8));
-
-            //Console.WriteLine(Square(5));
-
-            //Console.WriteLine(Cube(4));
-
-            //Console.WriteLine(Factorial(4));
-
-            //Console.WriteLine(Abs(-1));
-
-            //Console.WriteLine(Rand1());
-
-            //Console.WriteLine(Rand2());
-
-            //Console.WriteLine(Ceil(7.6f));
-
-            Console.WriteLine(Floor(8.2f));
-
+            Console.ReadKey();
         }
-
-
-
         public static bool IsOdd(int n)
         {
-
             if (n % 2 != 0)
             {
-
                 return true;
-
             }
-
             return false;
-
         }
-
-
-
-        public static bool IsEven(int n)
-        {
-
-            if (n % 2 == 0)
-            {
-
-                return true;
-
-            }
-
-            return false;
-
-        }
-
-
-
-        public static bool IsPrime(int n)
-        {
-
-            if (n <= 1)
-            {
-
-                return false;
-
-            }
-
-            for (int i = 2; i < n; i++)
-
-                if (n % i == 0)
-                {
-
-                    return false;
-
-                }
-
-            return true;
-
-        }
-
-
-
-        public static int Square(int n)
-        {
-
-            return n * n;
-
-        }
-
-
-
-        public static int Cube(int n)
-        {
-
-            return n * n * n;
-
-        }
-
-
-
-        public static int Pow(int x, int y)
-        {
-
-            int rs = 1;
-
-            for (int i = 1; i <= y; i++)
-
-                rs = rs * x;
-
-            return rs;
-
-        }
-
-
-
-        public static long Factorial(long n)
-        {
-
-            int f = 1;
-
-            for (int i = 2; i <= n; i++)
-
-                f = f * 1;
-
-            return f;
-
-        }
-
-
-
         public static int Abs(int n)
         {
-
             if (n < 0)
-
+            {
                 n = n * (-1);
-
+            }
             return n;
-
         }
-
-
-
-        public static int Rand1()
-        {
-
-            Random rd = new Random();
-
-            int num = rd.Next();
-
-            return num;
-
-        }
-
-
-
-        //public static float Rand2()
-
-        //{
-
-        //Random rd = new Random();
-
-        //float num = 
-
-        //return num;
-
-        //}
-
-
-
+        //input: float x
+        //ouput: int n that is smallest but not smaller than x
+        //idea: n=(int)x then n=n+1
+        //if x is negative then n is unchanged
+        //if x=a then n is unchanged
         public static int Ceil(float x)
         {
+            int n = (int)x;
+            if (x > 0)
+                if (x > n)
+                    n = n + 1;
+            return n;
 
-            if (x - (int)x == 0 || x < 0)
-            {
-
-                return (int)x;
-
-            }
-
-            return (int)(x) + 1;
-
+            //int d = (int)x;
+            //return d=d+1;
         }
-
-
-
+        public static int Cube(int n)
+        {
+            return n * n * n;
+        }
         public static int Floor(float x)
         {
-
-            return x >= 0 ? (int)x : (int)x - 1;
-
+            int a = (int)x;
+            if (x < 0 && x != a)
+                a = a - 1;
+            return a;
+            //int n = (int)x;
+            //if ((n - x) >= 0.5)
+            //    n = n ;
+            //return n;
+        }
+        public static bool IsEven(int n)
+        {
+            if (n % 2 == 0)
+                return true;
+            return false;
+        }
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+            for (int i = 2; i < n; i++) if (n % i == 0)
+                {
+                    return false;
+                }
+            return true;
+        }
+        public static int Pow(int x, int y)
+        {
+            int rs = 1;
+            for (int i = 1; i <= y; i++)
+                rs = rs * x;
+            return rs;
+        }
+        public static int Square(int n)
+        {
+            return n * n;
+        }
+        public static long Factorial(long n)
+        {
+            int F = 1;
+            for (int i = 2; i < n; i++)
+                F = F * i;
+            return F;
+        }
+        public static int Rand1()
+        {
+            Random rd = new Random();
+            int n = rd.Next();
+            return n;
+        }
+        public static float Rand2()
+        {
+            Random rd = new Random();
+            float r = rd.Next() % (0.1F) + (float)rd.Next(0, 1);
+            return r;
         }
 
     }
-
 }
--- hieunghia/UnitTestProject1/UnitTest1.cs
+++ hieunghia/UnitTestProject1/UnitTest1.cs
@@ -8,27 +8,75 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
+        public void TestIsOdd()
+        {
+            bool result3 = Program.IsOdd(3);
+            Assert.AreEqual(true, result3);
+
+            bool result4 = Program.IsOdd(4);
+            Assert.AreEqual(false, result4);
+        }
+        [TestMethod]
+        public void TestAbs()
+        {
+            int result6 = Program.Abs(-6);
+            Assert.AreEqual(6, result6);
+        }
+        [TestMethod]
         public void TestCeil()
         {
-            int result1 = Program.Ceil((float)2.3);
-            Assert.AreEqual(2, result1);
+            int rs = Program.Ceil(4.6F);
+            Assert.AreEqual(5, rs);
 
-            int result2 = Program.Ceil(-2.3f);
-            Assert.AreEqual(-2, result2);
+            int rs1 = Program.Ceil(-2.3F);
+            Assert.AreEqual(-2, rs1);
 
-            int result3 = Program.Ceil(3);
-            Assert.AreEqual(3, result3);
+            int rs2 = Program.Ceil(3);
+            Assert.AreEqual(3, rs2);
+
         }
+        [TestMethod]
         public void TestFloor()
         {
-            int result1 = Program.Floor((float)2.3);
-            Assert.AreEqual(2,result1);
-            int result2 = Program.Floor((float)-3.4);
-            Assert.AreEqual(-4, result2);
-            int result3 = Program.Floor(5);
-            Assert.AreEqual(5,result3);
+            int rs = Program.Floor(7.7F);
+            Assert.AreEqual(7, rs);
+        }
+        [TestMethod]
+        public void TestCube()
+        {
+            int rs = Program.Cube(-10);
+            Assert.AreEqual(-1000, rs);
+        }
+        [TestMethod]
+        public void TestIsEven()
+        {
+            bool rs = Program.IsEven(4);
+            Assert.AreEqual(true, rs);
+        }
+        [TestMethod]
+        public void TestIsPrime()
+        {
+            bool rs = Program.IsPrime(7);
+            Assert.AreEqual(true, rs);
+        }
+        [TestMethod]
+        public void TestPow()
+        {
+            int rs = Program.Pow(2, 3);
+            Assert.AreEqual(8, rs);
+        }
+        [TestMethod]
+        public void TestSquare()
+        {
+            int rs = Program.Square(4);
+            Assert.AreEqual(16, rs);
+        }
+        [TestMethod]
+        public void TestFactorial()
+        {
+            long rs = Program.Factorial(20L);
+            Assert.AreEqual(109641728, rs);
+        }
 
-        }
-        }
     }
 }
